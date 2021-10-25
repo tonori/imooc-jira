199677e35070d0eb5cc1eb731937c95fc3a7b2c4
Tochida tonori <tonori@tonori.cn>
2021-10-25 17:28:11 +0800

    feat: 添加新建看板组件空值验证

--- src/pages/boards/AddBoard.tsx
+++ src/pages/boards/AddBoard.tsx
@@ -1,18 +1,42 @@
 // Components
 import SubmitInput from "./SubmitInput";
 import { Container as BoardContainer } from "./BoardItem";
+import { Typography } from "antd";
 // Hooks
 import { useAddBoard } from "page-hooks/boards";
+import { useState } from "react";
 
 const AddBoard = () => {
-  const { mutate, isLoading } = useAddBoard();
+  const { mutateAsync: addBoard, isLoading } = useAddBoard();
+  const [inputValue, setInputValue] = useState("");
+  const [error, setError] = useState("");
+
+  const onSearch = () => {
+    if (inputValue === "") {
+      setError("任务看板名称不能为空");
+    } else {
+      if (error !== "") setError("");
+      addBoard(inputValue).then(() => {
+        setInputValue("");
+      });
+    }
+  };
+
   return (
     <BoardContainer>
       <SubmitInput
+        allowClear
         placeholder="新建任务看板"
         loading={isLoading}
-        onSearch={(value) => mutate(value)}
+        value={inputValue}
+        onChange={(e) => {
+          setInputValue(e.target.value);
+        }}
+        onSearch={onSearch}
       />
+      <Typography.Text type="danger" style={{ fontSize: "13px" }}>
+        {error}
+      </Typography.Text>
     </BoardContainer>
   );
 };
